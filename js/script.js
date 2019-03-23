1052d626f70ed33bdc090185f63d10dee16747aa
atinyakov <akoff16@gmail.com>
2019-03-23 15:47:13 +0300

    error animation

--- js/script.js
+++ js/script.js
@@ -10,6 +10,18 @@
   var popup = document.querySelectorAll('.modal');
   var close = document.querySelectorAll('.modal-close');
 
+  var form = document.querySelector('.modal-form');
+  var login = form.querySelector("[name=login]");
+  var password = form.querySelector("[name=password]");
+
+  var isStorageSupport = true;
+  var storage = "";
+
+  try {
+    storage = localStorage.getItem("login");
+  } catch (err) {
+    isStorageSupport = false;
+  }
 
   var openPopup = function (evt) {
     if(evt.target.tagName === 'IMG'){
@@ -17,15 +29,21 @@
     }
     if(evt.target.tagName === 'A'){
       writeUsPopup.classList.add('modal-show');
+      if (storage) {
+        login.value = storage;
+        password.focus();
+      } else {
+        login.focus();
+      }
     }
   };
 
   var closePopup =  function () {
     [].forEach.call(popup, function (el) {
       el.classList.remove('modal-show');
-    })
+      el.classList.remove("modal-error");
+    });
   };
-
 
   writeUsButton.addEventListener('click', function (evt) {
       evt.preventDefault();
@@ -53,6 +71,28 @@
          el.classList.remove("modal-show");
        }
      })
+   }
+ });
+
+ form.addEventListener("submit", function (evt) {
+   if (!login.value || !password.value) {
+     evt.preventDefault();
+     evt.target.classList.remove('modal-error');
+     setTimeout(function() { evt.target.classList.add("modal-error")}, 100);
+   } else {
+     if (isStorageSupport) {
+       localStorage.setItem("login", login.value);
+     }
+   }
+ });
+
+ window.addEventListener("keydown", function (evt) {
+   if (evt.keyCode === 27) {
+     evt.preventDefault();
+     if (popup.classList.contains("modal-show")) {
+       popup.classList.remove("modal-show");
+       popup.classList.add("modal-error");
+     }
    }
  });
 }())
